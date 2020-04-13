bb879b3991936ce66b7bf56323506513018ef13d
felixyin <ybkk1027@gmail.com>
2020-04-13 23:51:53 +0800

    首页样式调整

--- home/views.py
+++ home/views.py
@@ -22,17 +22,17 @@ class HomeView(DetailView):
         # kwargs['latest_blogs1'] = blogs[0:4]
         # kwargs['latest_blogs2'] = blogs[4:8]
         # kwargs['latest_blogs3'] = blogs[8:12]
-        kwargs['carousel_list'] = self.object.carousel_set.all()
-        kwargs['adware_list'] = self.object.adware_set.all()
-        kwargs['superiority_list'] = self.object.superiority_set.all()
+        kwargs['carousel_list'] = self.object.carousel_set.all().order_by('sequence')
+        kwargs['adware_list'] = self.object.adware_set.all().order_by('sequence')
+        kwargs['superiority_list'] = self.object.superiority_set.all().order_by('sequence')
 
-        kwargs['category_list'] = Category.objects.all()
+        kwargs['category_list'] = Category.objects.all().order_by('sequence')
         kwargs['case_list'] = Case.objects.all()[:6]
         product_list = Product.objects.all()
         kwargs['first_product'] = product_list[0]
         kwargs['other_products'] = product_list[0:4]
         kwargs['service_list'] = Service.objects.all()
-        kwargs['aboutitem_list'] = AboutItem.objects.all()
+        kwargs['aboutitem_list'] = AboutItem.objects.all().order_by('sequence')
         return super().get_context_data(**kwargs)
 
 
