1e5970407f79493fe546c3b6ba539d1beed94273
felixyin <ybkk1027@gmail.com>
2020-04-15 18:05:01 +0800

    服务器启动脚本

---
+++ gunicorn_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAME="QdqtrjWebsite"
+DJANGODIR=/root/qdqtrj_website #Django project directory
+USER=root # the user to run as
+GROUP=root# the group to run as
+NUM_WORKERS=2 # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=website.settings # which settings file should Django use
+DJANGO_WSGI_MODULE=website.wsgi # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source /root/qdqtrj_website/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec /root/qdqtrj_website/env/bin/gunicorn  ${DJANGO_WSGI_MODULE}:application \
+--name $NAME \
+--workers $NUM_WORKERS \
+--user=$USER --group=$GROUP \
+--log-level=debug \
+--log-file=-
