f3412f88ebd6a5a8a092f489b4ff4c9a58e91aaf
felixyin <ybkk1027@gmail.com>
2020-04-15 21:22:09 +0800

    update 脚本

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,15 +3,14 @@
 cd /root/qdqtrj_website
 
 # 备份数据库
-mysqldump -u root -h localhost -p"Ybkk1027" qdqtrj_website --complete-insert  --result-file="backup/qdqtrj_website.sql" --add-drop-trigger
---skip-add-locks --skip-lock-tables --skip-extended-insert
+mysqldump -u root -h localhost -p"Ybkk1027" qdqtrj_website --complete-insert --result-file="backup/qdqtrj_website$(date +%y-%m-%d_%H-%m-%S).sql" --add-drop-trigger --skip-add-locks --skip-lock-tables --skip-extended-insert
 
 # 提交文件到代码仓库
 git status
 git add .
 echo '要备份的文件:'
 git status
-git commit -m "backup_at_`date +%y-%m-%d_%H-%m-%S`"
+git commit -m "backup_at_$(date +%y-%m-%d_%H-%m-%S)"
 git status
 git push
 git status
