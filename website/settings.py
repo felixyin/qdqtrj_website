054b8c2d8a2602da381480e67c731485f6e72a57
felixyin <ybkk1027@gmail.com>
2020-04-16 18:08:36 +0800

    配置修改

--- website/settings.py
+++ website/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    # 'django.middleware.gzip.GZipMiddleware', # nginx 中已配置
+    'django.middleware.gzip.GZipMiddleware',  # nginx 中已配置
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
