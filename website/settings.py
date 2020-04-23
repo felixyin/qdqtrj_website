1a8158170a37cb1e36a5415970108deffc3b49a7
felixyin <ybkk1027@gmail.com>
2020-04-23 12:26:41 +0800

    editormd 插件支持拖拽上传图片、文件

--- website/settings.py
+++ website/settings.py
@@ -24,6 +24,9 @@ def env_to_bool(env, default):
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 源码方式安装第三方模块
+sys.path.insert(0, os.path.join(BASE_DIR, 'libs'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/zh-hans/3.0/howto/deployment/checklist/
 
@@ -341,35 +344,38 @@ CKEDITOR_CONFIGS = {
 }
 
 # Markdown 编辑器
-# MDEDITOR_CONFIGS = {
-#     'default': {
-#         'width': '100% ',  # Custom edit box width
-#         'heigth': 500,  # Custom edit box height
-#         'toolbar': ["undo", "redo", "|",
-#                     "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
-#                     "h1", "h2", "h3", "h5", "h6", "|",
-#                     "list-ul", "list-ol", "hr", "|",
-#                     "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table",
-#                     "datetime"
-#                     "emoji",
-#                     "html-entities", "pagebreak", "goto-line", "|",
-#                     "help", "info",
-#                     "||", "preview", "watch", "fullscreen"],  # custom edit box toolbar
-#         'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # image upload format type
-#         'image_floder': 'editor',  # image save the folder name
-#         'theme': 'default',  # edit box theme, dark / default
-#         'preview_theme': 'default',  # Preview area theme, dark / default
-#         'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
-#         'toolbar_autofixed': True,  # Whether the toolbar capitals
-#         'search_replace': True,  # Whether to open the search for replacement
-#         'emoji': True,  # whether to open the expression function
-#         'tex': False,  # whether to open the tex chart function
-#         'flow_chart': True,  # whether to open the flow chart function
-#         'sequence': True,  # Whether to open the sequence diagram function
-#         'mermaid': True,  # mermaid
-#         'vega': True,
-#     }
-# }
+MDEDITOR_CONFIGS = {
+    'default': {
+        'width': '100% ',  # Custom edit box width
+        'height': 500,  # Custom edit box height
+        'toolbar': ["undo", "redo", "|",
+                    "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+                    "h1", "h2", "h3", "h5", "h6", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table",
+                    "datetime"
+                    "emoji",
+                    "html-entities", "pagebreak", "goto-line", "|",
+                    "help", "info",
+                    "||", "preview", "watch", "fullscreen"],  # custom edit box toolbar
+        'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],  # image upload format type
+        'image_folder': 'editor',  # image save the folder name
+        'theme': 'default',  # edit box theme, dark / default
+        'preview_theme': 'default',  # Preview area theme, dark / default
+        'editor_theme': 'default',  # edit area theme, pastel-on-dark / default
+        'toolbar_autofixed': True,  # Whether the toolbar capitals
+        'search_replace': True,  # Whether to open the search for replacement
+        'emoji': True,  # whether to open the expression function
+        'tex': True,  # whether to open the tex chart function
+        'flow_chart': True,  # whether to open the flow chart function
+        'sequence': True,  # Whether to open the sequence diagram function
+        'mermaid': True,  # mermaid
+        'vega': True,
+        'watch': True,  # Live preview
+        'lineWrapping': False,  # lineWrapping
+        'lineNumbers': True  # lineNumbers
+    }
+}
 
 # simpleui 自定义菜单
 SIMPLEUI_STATIC_OFFLINE = True
