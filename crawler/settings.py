304380156fb46a350d33a5396264bb1d14c9b3f7
felixyin <ybkk1027@gmail.com>
2020-04-28 23:23:05 +0800

    jb51网站内容爬取 爬虫设置

--- crawler/settings.py
+++ crawler/settings.py
@@ -35,7 +35,7 @@ CONCURRENT_REQUESTS = 16
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0.3
 RANDOMIZE_DOWNLOAD_DELAY = True
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
 # The download delay setting will honor only one of:
