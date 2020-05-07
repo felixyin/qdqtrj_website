867844ad50a162c166298a73d6b36fc3014a1273
felixyin <ybkk1027@gmail.com>
2020-05-07 10:33:18 +0800

    爬虫配置性能调优

--- crawler/settings.py
+++ crawler/settings.py
@@ -27,20 +27,20 @@ NEWSPIDER_MODULE = 'crawler.spiders'
 # USER_AGENT = 'crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 20
+# CONCURRENT_REQUESTS = 20
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.12
+DOWNLOAD_DELAY = 0.15
 RANDOMIZE_DOWNLOAD_DELAY = True
 # USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 20
-CONCURRENT_REQUESTS_PER_IP = 20
+# CONCURRENT_REQUESTS_PER_DOMAIN = 20
+# CONCURRENT_REQUESTS_PER_IP = 20
 
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = True
